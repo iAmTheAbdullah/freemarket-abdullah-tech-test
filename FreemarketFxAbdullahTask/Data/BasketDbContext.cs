41dd11690afba25fa217a8fd68066a6722fa52be
Abdullah Malik <7953568+iAmTheAbdullah@users.noreply.github.com>
2025-11-07 06:28:55 +0000

    setup in-memory database context

---
+++ FreemarketFxAbdullahTask/Data/BasketDbContext.cs
@@ -0,0 +1,43 @@
+using FreemarketFxAbdullahTask.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreemarketFxAbdullahTask.Data;
+
+public class BasketDbContext : DbContext
+{
+    public BasketDbContext(DbContextOptions<BasketDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Basket> Baskets { get; set; }
+    public DbSet<BasketItem> BasketItems { get; set; }
+    public DbSet<DiscountCode> DiscountCodes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Basket>(entity =>
+        {
+            entity.HasKey(b => b.Id);
+            entity.Property(b => b.DiscountPercentage).HasPrecision(5, 2);
+        });
+
+        modelBuilder.Entity<BasketItem>(entity =>
+        {
+            entity.HasKey(bi => bi.Id);
+            entity.Property(bi => bi.Price).HasPrecision(18, 2);
+            entity.Property(bi => bi.DiscountPercentage).HasPrecision(5, 2);
+            entity.Property(bi => bi.ProductName).IsRequired().HasMaxLength(200);
+        });
+
+        modelBuilder.Entity<DiscountCode>(entity =>
+        {
+            entity.HasKey(dc => dc.Id);
+            entity.Property(dc => dc.Code).IsRequired().HasMaxLength(50);
+            entity.Property(dc => dc.Percentage).HasPrecision(5, 2);
+            entity.HasIndex(dc => dc.Code).IsUnique();
+        });
+    }
+}
+
